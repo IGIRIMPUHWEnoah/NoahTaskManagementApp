a904fd86aa6ff38652970c6bcd0d23de1e757c1e
IGIRIMPUHWEnoah <igirimpuhwenoah68@gmail.com>
2026-01-12 20:34:57 +0200

    generating secrete-key

--- src/main/java/noah/com/noahtaskapp/dtos/TokenResponse.java
+++ src/main/java/noah/com/noahtaskapp/dtos/TokenResponse.java
@@ -7,5 +7,6 @@ public class TokenResponse {
     private String token;
 
     public TokenResponse(String token) {
+        this.token=token;
     }
 }
--- src/main/java/noah/com/noahtaskapp/services/JwtService.java
+++ src/main/java/noah/com/noahtaskapp/services/JwtService.java
@@ -2,16 +2,19 @@ package noah.com.noahtaskapp.services;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
 @Service
 public class JwtService {
+@Value("${spring.jwt.secrete}")
+    private  String secrete;
 
     public  String generateToken(String email){
 
- return        Jwts.builder().setSubject(email).issuedAt(new Date()).expiration(new Date(System.currentTimeMillis()+1000*60*60*10)).signWith(Keys.hmacShaKeyFor("kabagambenaoh".getBytes())).compact();
+ return        Jwts.builder().setSubject(email).issuedAt(new Date()).expiration(new Date(System.currentTimeMillis()+1000*60*60*10)).signWith(Keys.hmacShaKeyFor(secrete.getBytes())).compact();
 
     }
 }
